93dbdab82d6f358f854d322f1728d5b8393ccf1a
Holger <Holger.Ulmer@gmx.de>
2021-09-03 14:07:11 +0200

    remote data to my gdrive

--- analyze.py
+++ analyze.py
@@ -7,3 +7,14 @@ print(df.head())
 
 print("Describe first feature")
 print(df['1'].describe())
+
+# workflow dvc = data versioning control
+# git is anhling the code and dvc handels the data
+# (1) create git repo
+# (2) dvc init  
+#     --> setup dvd
+# (3) dvc remote add -d myremote gdrive://1cfucAFBgmwtxiBoKC4PcntBHlDdouhox 
+#     --> setup a dvc remote, file on the local machine or google drive folder
+# (4) create a big data set locally
+# (5) dvc add data.csv
+
